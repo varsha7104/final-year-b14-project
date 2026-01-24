9703f132603156d40981ea2bbafba78336e7c5d1
J. Sri Hima Varsha <himavarsha710@gmail.com>
2026-01-24 11:27:19 +0530

    dashboard sidebar

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -167,7 +167,7 @@ export const SignInView=()=>{
         </form>
 </Form>
 
-        <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+        <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <p className="text-2xl font-semibold text-white">
                 <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]"></img>
                 Meet.AI</p></div>
