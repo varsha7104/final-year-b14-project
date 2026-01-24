5674ec79b3ad24f06b04357786481771e0412388
J. Sri Hima Varsha <himavarsha710@gmail.com>
2026-01-24 15:28:40 +0530

    agent setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import {  useSuspenseQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
+import { LoadingState } from "@/components/loading-state";
+import { ErrorState } from "@/components/error-state";
+
+export const AgentsView = () => {
+  const trpc = useTRPC();
+
+  const { data } =
+    useSuspenseQuery(trpc.agents.getMany.queryOptions());
+
+  
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+export const AgentsViewLoading = () => {
+    return <LoadingState title="Loading Agents" description="This may take a few seconds" />;
+};
+
+export const AgentsViewError = () => {
+    return <ErrorState title="Failed to load Agents" description="There was an error while fetching agents. Please try again." />;
+};
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,26 +1,10 @@
 "use client"
-import { Button } from "@/components/ui/button";
-
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-
-
-
-
 
  export const HomeView= () => {
-    const router=useRouter();
-  const { data: session } = authClient.useSession();
-if(!session){
-  return (<p>
-  Loading</p>)
-}
+   
   return(
-    <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session.user.name}</p>
-      <Button onClick={()=>authClient.signOut({fetchOptions:{onSuccess:()=>router.push("/sign-in")
-
-      }})}>SignOut</Button>
+    <div >
+      
       Home Page
     </div>
   )};
