9bf38c91f6ab3f498e19500d1294fff857e390f5
J. Sri Hima Varsha <himavarsha710@gmail.com>
2026-02-03 21:28:51 +0530

    16 meetings setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 
 import {agentsRouter} from '@/modules/agents/server/procedure';
 import {  createTRPCRouter } from '../init';
+import { meetingsRouter } from '@/modules/meetings/server/procedure';
 export const appRouter = createTRPCRouter({
     agents: agentsRouter,
+    meetings: meetingsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
